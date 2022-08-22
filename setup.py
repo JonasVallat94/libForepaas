6f4c9d340f68144bc45c430ffb6dbea3e5a3b6f3
JonasVallat <jvallat@synotis.ch>
2022-08-22 14:58:44 +0200

    changed name

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
+    name='libForepaas',
+    version='0.2.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
     author='Kenneth Reitz',
